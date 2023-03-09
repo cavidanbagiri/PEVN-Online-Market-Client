9b53d30b8cbf231910cf68779c42d106222816f1
cavidanbagiri <cavidanbagiri@gmail.com>
2023-03-09 05:00:58 +0300

    Add Basket and Favorites

--- src/components/ProductComponents/CardItem_Comp.vue
+++ src/components/ProductComponents/CardItem_Comp.vue
@@ -6,16 +6,28 @@
             <span>Brand : {{ prop.product.brand }}</span>
             <br>
             <span>Model : {{ prop.product.model }}</span>
-    
+            <br>
+            
         </div>
     </router-link>
+    <button class="border-2 bg-black text-white" @click="addBasket">
+        Add Basket
+    </button>
+    <button class="border-2 bg-red-500 text-white" @click="addFavorites">
+        Add Favorite
+    </button>
 </template>
 
 <script setup>
 
+    import productStore from '../../store/product_store';
+
+    const store = productStore();
+
     const prop = defineProps(['product']);
 
-
+    const addBasket = () => store.ADDBASKET(prop.product.id);
+    const addFavorites = () => store.ADDFAVORITES(prop.product.id);
 
 </script>
 
